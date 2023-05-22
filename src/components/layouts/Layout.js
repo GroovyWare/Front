ab4b1dfa714b67fadca9e9c5545a8e112724606c
yuchan Choi <ucc104593@gmail.com>
2023-05-22 14:55:42 +0900

    commit

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,18 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../common/Header";
-import Footer from "../common/Footer";
-import Navbar1 from "../common/Navbar1";
+import Navbar from "../common/Navbar"
 import LayoutCSS from "../layouts/Layout.module.css";
-import Header1 from "../common/Header1";
+
 
 
 
 function Layout(){
     return(
             <div className={ LayoutCSS.container }>
-                    <Header1/>
+                    <Header/>
                 <div className={ LayoutCSS.content}>
-                    <Navbar1/>
+                    <Navbar/>
                     <Outlet className={ LayoutCSS.contentBg }/>
                 </div>
             </div>
