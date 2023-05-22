829d7be86fa465cd44ee9f150177759cd17053f5
yuchan Choi <ucc104593@gmail.com>
2023-05-22 14:28:29 +0900

    commit

--- src/components/common/PagingBar.js
+++ src/components/common/PagingBar.js
@@ -14,6 +14,11 @@ function PagingBar({pageInfo, setCurrentPage}){
         <div style={ {listStyleType : 'none', display : 'flex', justifyContent : 'center'} }>
             <button 
             className={PagingBarCSS.pagingBtn}
+            onClick={() => setCurrentPage(pageInfo.startPage)}
+            disabled={pageInfo.currentPage <= 1}
+            >&lt;&lt;</button>
+            <button 
+            className={PagingBarCSS.pagingBtn}
             onClick={() => setCurrentPage(pageInfo.currentPage - 1)}
             disabled={pageInfo.currentPage <= 1}
             >&lt;</button>
@@ -28,6 +33,10 @@ function PagingBar({pageInfo, setCurrentPage}){
             className={PagingBarCSS.pagingBtn}
             onClick={() => setCurrentPage(pageInfo.currentPage + 1)}
             disabled={pageInfo.currentPage >= pageInfo.maxPage}>&gt;</button>
+            <button 
+            className={PagingBarCSS.pagingBtn}
+            onClick={() => setCurrentPage(pageInfo.endPage)}
+            disabled={pageInfo.currentPage >= pageInfo.maxPage}>&gt;&gt;</button>
         </div>
     )
 }
