a7aea300339fc772e3cf8996e01b403852e78226
yuchan Choi <ucc104593@gmail.com>
2023-05-26 17:05:31 +0900

    [최유찬] 직원 정보 수정 기능 개발중

--- src/modules/EmployeeModule.js
+++ src/modules/EmployeeModule.js
@@ -5,18 +5,21 @@ const initialState = [];
 const GET_EMPLOYEES = 'employee/GET_EMPLOYEES';
 const GET_EMPLOYEE = 'employee/GET_EMPLOYEE';
 const POST_EMPLOYEE = 'employee/POST_EMPLOYEE';
+const PUT_EMPLOYEE = 'employee/PUT_EMPLOYEE';
 
-export const { employee : { getEmployees, getEmployee, postEmployee  }} = createActions({
+export const { employee : { getEmployees, getEmployee, postEmployee, putEmployee  }} = createActions({
     [GET_EMPLOYEES] : res => res.data,
     [GET_EMPLOYEE] : res => res.data,
-    [POST_EMPLOYEE] : res => res
+    [POST_EMPLOYEE] : res => res,
+    [PUT_EMPLOYEE] : res => res
 });
 
 const employeeReducer = handleActions(
     {
         [GET_EMPLOYEES] : (state, { payload }) => payload,
         [GET_EMPLOYEE] : (state, { payload }) => payload,
-        [POST_EMPLOYEE] : (state, { payload }) => ({ regist : payload })
+        [POST_EMPLOYEE] : (state, { payload }) => ({ regist : payload }),
+        [PUT_EMPLOYEE] : (state, { payload }) => ({ update : payload })
     }
 
 ,initialState);
